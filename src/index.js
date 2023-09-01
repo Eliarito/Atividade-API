8f369d110c7cc3acaf8e88a09466a256e3d5a232
Eliarito <133966592+Eliarito@users.noreply.github.com>
2023-09-01 13:42:11 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 import dotenv from "dotenv";
 import express from "express";
 dotenv.config();
+import routerUsuario from "./routes/usuario";
 
 const app = express(); 
 const port = 3000; 
 
 app.use(express.json());
-
+app.use(routerUsuario);
 app.get("/", (req, res) => {
   console.log("Rota / solicitada");
   res.json({
